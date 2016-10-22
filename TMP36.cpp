4e224be6cdc2d5c16091db953b7f8c778bdfca4b
Gaetano Fresa <fischkotpf@hotmail.de>
2016-10-22 15:08:43 +0200

    Update to TMP36.cpp
    added possibility to user special ADC VCC

--- TMP36.cpp
+++ TMP36.cpp
@@ -1,5 +1,5 @@
 /*
-  TMP36.cpp - Library for flashing Morse code.
+  TMP36.cpp - Library for TMP36 temperature sensor.
   Created by Gaetano, May 25, 2016.
   Released into the public domain.
 */
@@ -7,16 +7,17 @@
 #include "Arduino.h"
 #include "TMP36.h"
 
-TMP36::TMP36(int pin)
+TMP36::TMP36(int pin, float vcc)
 {
   //pinMode(pin, INPUT);
   _pin = pin;
+  _vcc = vcc;
 }
 
 float TMP36::getTempC()
 {
   int reading = analogRead(_pin);
-   float voltage = reading * 5.0;
+   float voltage = reading * _vcc;
   voltage /= 1024.0; 
   float temperatureC = (voltage - 0.5) * 100;
   return temperatureC;
@@ -25,7 +26,7 @@ float TMP36::getTempC()
 float TMP36::getTempF()
 {
 	int reading = analogRead(_pin);
-   float voltage = reading * 5.0;
+   float voltage = reading * _vcc;
   voltage /= 1024.0; 
   float temperaturec = (voltage - 0.5) * 100;
   float temperatureF = (temperaturec * 9.0 / 5.0) + 32.0;
